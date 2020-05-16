4d4bb621c0d1ae91bd3e86057eec8dfc8bcebf20
vicanso <vicansocanbico@gmail.com>
2020-05-16 15:08:55 +0800

    refactor: enhance error handler function

---
+++ web/src/constants/env.js
@@ -0,0 +1,9 @@
+const env = process.env.NODE_ENV
+
+export function isDevelopment () {
+  return env === 'development'
+}
+
+export function isProduction () {
+  return env === 'production'
+}
--- web/src/main.js
+++ web/src/main.js
@@ -6,17 +6,34 @@ import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 import '@/main.sass'
+import {
+  isDevelopment
+} from '@/constants/env'
 
-Vue.use(Element)
 Vue.config.productionTip = false
 Vue.config.errorHandler = function (err, vm, info) {
   // TODO 错误异常上报
+  const {
+    tag
+  } = vm.$vnode.componentOptions
   console.error(err)
+  console.info(vm)
+  console.dir(tag)
+  console.info(info)
+  // 如果开发环境，弹alert
+  if (isDevelopment()) {
+    alert(`${tag} ${info}`)
+  }
 }
 window.onerror = function (msg, url, lineNo, columnNo, err) {
   // TODO 错误异常上报
-  this.console.error(err)
+  console.error(err)
 }
+Vue.config.keyCodes = {
+  // 回车
+  enter: 0x0d
+}
+Vue.use(Element)
 
 new Vue({
   router,
